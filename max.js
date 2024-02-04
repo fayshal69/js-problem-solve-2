0fea17b11d5a9fa4eadec074e2d955cd6a83f585
fayshal-bin-amir <foyshalbinamir@gmail.com>
2024-02-04 18:23:44 +0600

    added max.js

---
+++ max.js
@@ -0,0 +1,12 @@
+const disha = 56;
+const salman = 66;
+
+if(disha > salman) {
+    console.log('Disha will eat the stoebery');
+}
+else {
+    console.log('Salman will eat the stoebery');
+}
+
+const max = Math.max(12, 0, -2, 5, 9, 7);
+console.log(max);
